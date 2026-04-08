2574f6b5330bae12519a45f973f2493174226d0b
MaironBarquero1 <maironucr@gmail.com>
2026-04-08 15:30:14 -0600

    Refactor project structure and add initial domain entities and DbContext for product management

---
+++ src/StoreBackend.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreBackend.Domain.Entities;
+
+[Table("Product")]
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int ProductId { get; private set; }
+
+    [Required]
+    public Guid ProductResourceId { get; set; }
+
+    [Column("Name")]
+    [StringLength(50)]
+    public string? Name { get; set; }
+}
